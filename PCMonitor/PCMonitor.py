0ca00bef7ee6a4f33c49608985326d9e77aa44e5
Rxann <80224521+Rxann@users.noreply.github.com>
2021-05-12 13:23:18 -0400

    ver. 1.0.0

--- PCMonitor/PCMonitor.py
+++ PCMonitor/PCMonitor.py
@@ -17,4 +17,4 @@ while True:  # The presence will stay on as long as the program is running
     mem = psutil.virtual_memory()
     mem_per = round(psutil.virtual_memory().percent,1)
     RPC.update(details="RAM: "+str(mem_per)+"%", state="CPU: "+str(cpu_per)+"%")  # Set the presence
-    time.sleep(10) # Can only update rich presence every 15 seconds
+    time.sleep(5) # Can only update rich presence every 5 seconds
